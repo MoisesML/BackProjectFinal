028255126e78e9832fa9d290b08eb6b384b5e3ca
MoisesML <69059086+MoisesML@users.noreply.github.com>
2021-01-09 06:56:53 -0500

    deploy6

--- dist/routes/postulacion.js
+++ dist/routes/postulacion.js
@@ -5,7 +5,7 @@ const express_1 = require("express");
 const postulacion_1 = require("../controllers/postulacion");
 const Validador_1 = require("../utils/Validador");
 exports.postulacion_router = express_1.Router();
-exports.postulacion_router.post('/postulante/:id', postulacion_1.agregarPostulante);
+exports.postulacion_router.post('/postulante/:id', Validador_1.controladorPersona, postulacion_1.agregarPostulante);
 exports.postulacion_router.get('/postulantes/anuncio/:id', postulacion_1.postulacionAnuncio);
 exports.postulacion_router.get('/postulaciones/persona/:id', Validador_1.controladorPersona, postulacion_1.postulacionesPersona);
 exports.postulacion_router.get('/postulacion/:id/:idAn', Validador_1.controladorPersona, postulacion_1.postulacionExacta);
--- routes/postulacion.ts
+++ routes/postulacion.ts
@@ -4,7 +4,7 @@ import { controladorPersona } from "../utils/Validador";
 
 export var postulacion_router = Router();
 
-postulacion_router.post('/postulante/:id', agregarPostulante);
+postulacion_router.post('/postulante/:id',controladorPersona, agregarPostulante);
 postulacion_router.get('/postulantes/anuncio/:id', postulacionAnuncio);
 postulacion_router.get('/postulaciones/persona/:id',controladorPersona, postulacionesPersona);
 postulacion_router.get('/postulacion/:id/:idAn', controladorPersona, postulacionExacta);
